b62cad4eec5fa743de5e3b73c5f959459827d886
Tai Chen <mindlikewater@gmail.com>
2016-10-11 15:45:34 -0400

    Functioning clock without colors

---
+++ main.js
@@ -0,0 +1,25 @@
+function getTime () {
+  var time = new Date ();
+  var hours = time.getHours ();
+  var min = time.getMinutes ();
+  var sec = time.getSeconds ();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+
+  var currentTime = hours + ":" + min + ":" + sec;
+
+  displayTime.innerHTML = currentTime;
+};
+
+setInterval(getTime, 1000);
+
+var displayTime = document.getElementById("time");
+getTime();
